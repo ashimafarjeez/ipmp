6b1b0f261dbe537707443c74149316f6301a8e1f
Ashima Farjeez <ashimafarjeez@gmail.com>
2026-01-09 08:06:45 +0530

    one_odd_occurrence

---
+++ one_odd_occurrence.cpp
@@ -0,0 +1,13 @@
+//Given an array of arr[] positive integers where all numbers occur even number of times 
+//except one number which occurs odd number of times. Return that number
+
+class Solution {
+  public:
+    int getOddOccurrence(vector<int>& arr) {
+        int xor_arr = 0;
+        for(int a: arr){
+            xor_arr^=a;
+        }
+        return xor_arr;
+    }
+};
